a21077b8fce2b295b47a0a94b511933c682a8276
Gulera Israilova <guleraisrailova@gmail.com>
2022-06-15 06:13:08 +0600

    WUS-134:add crud for sportsmen

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { DB_CONFIG } from './utils/db_config';
 import { UsersModule } from './users/users.module';
 import { ClubsModule } from './clubs/clubs.module';
 import { MailModule } from './services/mail/mail.module';
+import { SportsmenModule } from './sportsmen/sportsmen.module';
+import { S3Module } from './s3/s3.module';
 
 @Module({
   imports: [
@@ -11,6 +13,8 @@ import { MailModule } from './services/mail/mail.module';
     UsersModule,
     ClubsModule,
     MailModule,
+    SportsmenModule,
+    S3Module,
   ],
 })
 export class AppModule {}
--- src/users/dto/user.payload.ts
+++ src/users/dto/user.payload.ts
@@ -2,7 +2,7 @@ import { PickType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
 export class UserPayload extends PickType(CreateUserDto, [
-  'id' as const,
+  //'id' as const,
   'name' as const,
   'email' as const,
 ]) {}
