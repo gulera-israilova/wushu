5540317d18dc0bc9edd530ac204598615abfd361
pratovv <timur.pratovv@gmail.com>
2022-07-14 10:11:15 +0600

    added status in payload

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { EventsModule } from './events/events.module';
 import { MessagesModule } from './messages/messages.module';
 import { DirectModule } from './direct/direct.module';
 import { LobbyModule } from './lobby/lobby.module';
-import {CloudinaryModule} from "./services/cloudinary/cloudinary.module";
+import { CloudinaryModule } from './services/cloudinary/cloudinary.module';
 
 @Module({
   imports: [
@@ -26,7 +26,8 @@ import {CloudinaryModule} from "./services/cloudinary/cloudinary.module";
     MessagesModule,
     DirectModule,
     LobbyModule,
-      CloudinaryModule
+    CloudinaryModule,
   ],
 })
 export class AppModule {}
+ 
